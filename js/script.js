f8d4d05121393a1ff1d9d9265343fe6446604108
Shivani Saroj <shivanis.ug22.cs@nitp.ac.in>
2024-04-07 11:42:12 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
+
+// changing search icon image on window resize
+window.addEventListener('resize', changeSearchIcon);
+function changeSearchIcon(){
+    let winSize = window.matchMedia("(min-width: 1200px)");
+    if(winSize.matches){
+        document.querySelector('.search-icon img').src = "images/search-icon.png";
+    } else {
+        document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
+    }
+}
+changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+window.addEventListener('resize', () =>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
+
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzV8ASXrCCnqT5jZA3-6EbGbobAFgko_fRTH5N-5rt3PfnVoR5D63B7BYllQkFyM1QwZw/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+
+
+ 
+
+  
+  function abc(){
+document.getElementById("form").style.display=none;
+alert("We will contact you soon");
+  }
+
+     
+
+  
